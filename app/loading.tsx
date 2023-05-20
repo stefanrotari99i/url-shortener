a5ecc1b777c3a34aef3c0561e45c065a65a144c5
stefanrotari99i <stefan.rotari112@gmail.com>
2023-05-20 13:13:00 +0300

    error handler

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <div className='w-screen h-screen flex justify-center items-center'>
+            <div className='loader'></div>
+        </div>
+    )
+}
