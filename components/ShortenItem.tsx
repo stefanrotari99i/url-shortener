914680a4bf43d7bf3b06263ef38811db249d9ae4
stefanrotari99i <stefan.rotari112@gmail.com>
2023-05-23 13:22:34 +0300

    fix sorten item

--- components/ShortenItem.tsx
+++ components/ShortenItem.tsx
@@ -1,25 +1,47 @@
+"use client";
 import { FiExternalLink } from "react-icons/fi";
 import { HiCursorClick } from "react-icons/hi";
 import { BiCopy } from "react-icons/bi";
 import { timeAgo } from "@/utils/timeAgo";
-
+import { useEffect, useState } from "react";
 interface ShortenItemProps {
     item: any;
 }
 
 const ShortenItem = ({ item }: ShortenItemProps) => {
+    const [location, setLocation] = useState("");
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(`${location}/r/${item.shortUrl}`);
+    };
+
+    useEffect(() => {
+        setLocation(window.location.origin);
+    }, []);
+
     return (
         <article className="overflow-hidden rounded-xl p-6 w-full bg-[#0d0d0d]/80 backdrop-blur-md hover:bg-[#0d0d0d]/100 transition duration-300">
             <div className="flex flex-col items-start gap">
-                <a href={item.longUrl} className="text-neutral-300 text-sm line-clamp-1 break-all">
+                <a
+                    href={item.longUrl}
+                    className="text-neutral-300 text-sm line-clamp-1 break-all"
+                >
                     {item.longUrl}
                 </a>
                 <div className="flex items-center gap-3">
-                    <a href={`http://localhost:3000/r/${item.shortUrl}`} className="text-white font-bold text-md" target="_blank">
-                        {`localhosts/${item.shortUrl}`}
+                    <a
+                        href={`${location}/r/${item.shortUrl}`}
+                        className="text-white font-bold text-md"
+                        target="_blank"
+                    >
+                        {`${location}/r/${item.shortUrl}`}
                     </a>
-                    <a href={`http://localhost:3000/r/${item.shortUrl}`} target="_blank" className="bg-violet-500/10 h-8 w-8 flex items-center justify-center rounded-xl">
-                    <FiExternalLink className="text-violet-500 text-sm" />
+                    <a
+                        href={`${location}/r/${item.shortUrl}`}
+                        target="_blank"
+                        className="bg-violet-500/10 h-8 w-8 flex items-center justify-center rounded-xl"
+                    >
+                        <FiExternalLink className="text-violet-500 text-sm" />
                     </a>
                 </div>
             </div>
@@ -35,7 +57,7 @@ const ShortenItem = ({ item }: ShortenItemProps) => {
                         {timeAgo(item.createdAt)}
                     </p>
                 </div>
-                <button className="bg-transparent text-violet-500 hover:text-violet-700 focus:outline-none active:scale-105">
+                <button className="bg-transparent text-violet-500 hover:text-violet-700 focus:outline-none active:scale-105" onClick={copyToClipboard}>
                     <BiCopy className="text-lg" />
                 </button>
             </div>
